0434a885d88d7cbc470ddca674e48c5f76aeb06e
camila <mariacamilaluango@gmail.com>
2024-10-19 22:10:18 -0500

    operacion desc prenda

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 let precio = parseInt (prompt ("cual es el precio de la prenda?"))
 let desc = parseInt (prompt ("de cuanto es el descuento que desea agregar?"))
+let res =(precio*desc)/100, total=precio-res
