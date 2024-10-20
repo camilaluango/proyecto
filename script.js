8cb44894db5bfdc6cfcdbf9f2d11bbd6bac62bd7
camila <mariacamilaluango@gmail.com>
2024-10-19 22:16:26 -0500

    inicio ejercicio 2

--- script.js
+++
@@ -1,7 +0,0 @@
-let precio = parseInt (prompt ("cual es el precio de la prenda?"))
-let desc = parseInt (prompt ("de cuanto es el descuento que desea agregar?"))
-let res =(precio*desc)/100, total=precio-res
-
-document.write("valor de la prenda: ","$", precio,"<br>")
-document.write("con un descuento del: ",desc,"%","<br>")
-document.write("el total a pagar es: ","$", total)
