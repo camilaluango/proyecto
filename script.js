eab7f13c4f8e9c3ecf6fce54fdd1ee7de2d04997
camila <mariacamilaluango@gmail.com>
2024-10-19 22:38:04 -0500

    funcion potencia con ciclo for

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let base =parseInt(prompt("ingresa el numero base"))
 let exp = parseInt (prompt("ingresa el numero exponente"))
+
+function potencia(base , exp){
+    let resultado = 1
+
+    for (let i = 0; i < exp; i++){
+        resultado *= base;
+    }
+    return resultado;
+}
