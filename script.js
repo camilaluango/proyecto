314ceadeb81f5896d5ff72e6f314d445ad9a2dc0
camila <mariacamilaluango@gmail.com>
2024-10-19 22:20:33 -0500

    resultado promedio con console.log

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ let nota2 = parseInt(prompt("ingresa la segunda nota"))
 let nota3 = parseInt(prompt("ingresa la tercera nota"))
 
 let prom = (nota1+nota2+nota3)/3
+
+console.log("su promedio es de: ", prom)
