11c7716dd3eda29a60f608afb392c9df1836b137
camila <mariacamilaluango@gmail.com>
2024-10-19 22:27:10 -0500

    inicio ejercicio 3

--- script.js
+++
@@ -1,7 +0,0 @@
-let nota1 = parseInt(prompt("ingresa la primera nota"))
-let nota2 = parseInt(prompt("ingresa la segunda nota"))
-let nota3 = parseInt(prompt("ingresa la tercera nota"))
-
-let prom = (nota1+nota2+nota3)/3
-
-console.log("su promedio es de: ", prom)
