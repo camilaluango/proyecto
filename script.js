92656fe26389056393410f505adda67cbeebe5fb
camila <mariacamilaluango@gmail.com>
2024-10-22 21:45:14 -0500

    todos los ejercicios

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-let base =parseInt(prompt("ingresa el numero base"))
+document.write("ejercicio 1")
+
+let precio = parseInt (prompt ("cual es el precio de la prenda?"))
+let desc = parseInt (prompt ("de cuanto es el descuento que desea agregar?"))
+let res =(precio*desc)/100, total=precio-res
+
+document.write("valor de la prenda: ","$", precio,"<br>")
+document.write("con un descuento del: ",desc,"%","<br>")
+document.write("el total a pagar es: ","$", total)
+
+
+document.write("ejercicio 2")
+
+let nota1 = parseInt(prompt("ingresa la primera nota"))
+let nota2 = parseInt(prompt("ingresa la segunda nota"))
+let nota3 = parseInt(prompt("ingresa la tercera nota"))
+let prom = (nota1+nota2+nota3)/3
+
+console.log("su promedio es de: ", prom)
+
+document.write("ejercicio 3")
+
+console.log("area de un triangulo")
+let base= parseInt(prompt("cuanto mide la base?"))
+let alt= parseInt(prompt("cuanto tiene de altura?"))
+let tot = (base*alt)/2
+
+document.write(`el area del triangulo es:  ${tot}`)
+
+document.write("ejercicio 4")
+
+let bas =parseInt(prompt("ingresa el numero base"))
 let exp = parseInt (prompt("ingresa el numero exponente"))
 
-function potencia(base , exp){
+function potencia(bas , exp){
     let resultado = 1
 
     for (let i = 0; i < exp; i++){
